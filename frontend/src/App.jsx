2edf4b33732f42414edb2abcb8c34f24dad88d94
Americano.Dev <andrenations@gmail.com>
2024-05-09 21:41:14 -0400

    added the card and map the the data to display it on the page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,26 @@
-import TextCenter from './components/TextCenter'
+import TextCenter from './components/TextCenter';
+import cardData from './data';
 
 const App = () => {
   return (
     <div>
       <TextCenter title="ATTENTION! You do not need JS for everything!" />
+      <div className="container">
+        {cardData.map((card, index) => (
+          <div className={`card ${card.backgroundColor}`} key={index}>
+            <div className="card-inner">
+              <div className="card-front">
+                <img src={card.frontImg} alt="" />
+              </div>
+              <div className="card-back">
+                <img src={card.backImg} alt="Back" />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
