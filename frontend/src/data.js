f6b1e0ee011294667164e33d3b1b7ad7a582df27
Americano.Dev <andrenations@gmail.com>
2024-05-09 21:24:43 -0400

    added data

---
+++ frontend/src/data.js
@@ -0,0 +1,25 @@
+import img1 from './assets/pic1.png'
+import img2 from './assets/pic2.png'
+import img3 from './assets/pic3.png'
+
+const cardData = [
+  {
+    frontImg: img1,
+    backImg: img3,
+    backgroundColor: 'yellow-card', 
+  },
+  {
+    frontImg: img3,
+    backImg: img2,
+    backgroundColor: 'green-card', 
+  },
+  {
+    frontImg: img2,
+    backImg: img1,
+    backgroundColor: 'blue-card', 
+  },
+  
+];
+
+
+export default cardData;
