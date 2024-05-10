8cd129d5b7ff3997a167af18a24e379a5ed61060
Americano.Dev <andrenations@gmail.com>
2024-05-09 21:36:53 -0400

    added the TextCenter component

---
+++ frontend/src/components/TextCenter.jsx
@@ -0,0 +1,23 @@
+
+const TextCenter = ({title}) => {
+  return (
+    <div className="text-center">
+    <h1 className="title">{title}</h1>
+      <h5>
+        Flipping cards with <span className="letter-gold">C</span>
+        <span className="letter-orange">S</span>
+        <span className="letter-purple">S</span> is a simple and effective way to add
+      </h5>
+      <p>
+        interactivity to your website or application. By combining <span className="small-gold">C</span>
+        <span className="small-orange">S</span>
+        <span className="small-purple">S</span>
+      </p>
+      <p>transitions and transforms, you can create engaging user experiences </p>
+      <p>that captivate your audience. Experiment with different styles and </p>
+      <p>effects to customize your flipping cards to suit your design needs.</p>
+    </div>
+  );
+};
+
+export default TextCenter;
